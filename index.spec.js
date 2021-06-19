39cbde9431c82b435d7b35504dc685e9d90380a1
m-sureshraj <m.s.suresh100@gmail.com>
2021-06-19 19:53:21 +0530

    fix: make it pass

--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,7 @@ const { sayHello, upperCase } = require('./index');
 
 describe('sayHello', () => {
   it('should say hello', () => {
-    expect(sayHello()).toBe('Hell');
+    expect(sayHello()).toBe('Hello');
   });
 
   it('should format string to uppercase', () => {
